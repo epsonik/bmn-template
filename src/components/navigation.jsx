acb191476aff54d7c05cc0ab1ecc6061b07cbf18
mateuszb <mateusz.b@aibusters.com>
2022-10-04 00:00:17 +0200

    t

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,43 +1,45 @@
 export const Navigation = (props) => {
-  return (
-    <nav id='menu' className='navbar navbar-expand-lg navbar-light navbar-fixed-top'>
-      <div className='container'>
-        <div className='navbar-header'>
-          <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='#bs-example-navbar-collapse-1'
-          >
-            {' '}
-            <span className='sr-only'>Toggle navigation</span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-          </button>
-          <a className='navbar-brand page-scroll' href='#page-top'>
-            React Landing Page
-          </a>{' '}
-        </div>
+    return (
+        <nav id='menu' className='navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top py-lg-0 '>
+            <div className='container'>
+                <div className='navbar-header'>
+                    <button
+                        type='button'
+                        className='navbar-toggle collapsed'
+                        data-toggle='collapse'
+                        data-target='#bs-example-navbar-collapse-1'
+                    >
+                        {' '}
+                        <span className='sr-only'>Toggle navigation</span>{' '}
+                        <span className='icon-bar'></span>{' '}
+                        <span className='icon-bar'></span>{' '}
+                        <span className='icon-bar'></span>{' '}
+                    </button>
+                    <div className='navbar-brand page-scroll' href='#page-top'>
+                        <a class="navbar-brand"  href='#'>
+                        <img src="img/PNG/MBN_podstawowy_zloty_poziom.png" width="300" height="300"/>
+                        </a>
+                    </div>{' '}
+                </div>
 
-        <div
-          className='collapse navbar-collapse'
-          id='bs-example-navbar-collapse-1'
-        >
-          <ul className='nav navbar-nav navbar-right'>
-            <li>
-              <a href='#team' className='page-scroll'>
-                Team
-              </a>
-            </li>
-            <li className='contact.social'>
-              <a href={props.data ? props.data.facebook : '/'}>
-              <i className='fa fa-facebook'></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
+                <div
+                    className='collapse navbar-collapse'
+                    id='bs-example-navbar-collapse-1'
+                >
+                    <ul className='nav navbar-nav navbar-right'>
+                        <li>
+                            <a href='#team' className='page-scroll'>
+                                Team
+                            </a>
+                        </li>
+                        <li className='contact.social'>
+                            <a href={props.data ? props.data.facebook : '/'}>
+                                <i className='fa fa-facebook'></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
 }
